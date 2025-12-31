50004a91ff90f8297f1c726aaeaf545d51f2ef10
HungNguyen <Edwardnguyen196@gmail.com>
2026-01-01 00:56:35 +0100

    add gitignore

--- HappyHome.Domain/obj/Debug/net8.0/HappyHome.Domain.AssemblyInfo.cs
+++ HappyHome.Domain/obj/Debug/net8.0/HappyHome.Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HappyHome.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3e7e2ff0e711250b989e269f01b5b19147e742b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+609346015044a4745290a4b41cc4b629fbf57b39")]
 [assembly: System.Reflection.AssemblyProductAttribute("HappyHome.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HappyHome.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
